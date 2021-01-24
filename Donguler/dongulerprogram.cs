1a4378b4035d4bbf7e1cc0b14fa5f535eb2729bd
Sadık <sadiksezgin1702@hotmail.com>
2021-01-24 15:25:29 +0300

    20 Ocak Ders Sonu

---
+++ Donguler/dongulerprogram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Donguler
+{
+    class dongulerprogram
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Proglamlamaya başlangıç temel kurs";
+            string kurs3 = "Java";
+            string kurs4 = "Python";
+            string kurs5 = "C++";
+
+            Console.WriteLine(kurs1);
+            Console.WriteLine(kurs2);
+            Console.WriteLine(kurs3);
+            Console.WriteLine(kurs4);
+            Console.WriteLine(kurs5);
+
+            //array - dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", 
+                "Proglamlamaya başlangıç temel kurs", 
+                "Java","Python","C#" };
+
+
+            for (int i = 0; i<kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            
+            Console.WriteLine("sayfasonu - footer");
+        }
+    }
+}
